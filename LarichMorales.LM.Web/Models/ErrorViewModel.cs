9aec94863dfe09190cac10e4843316e43618b974
COMLABPC11-PC\COMLABPC11 <kelvinjoe0911@gmail.com>
2018-08-09 18:28:31 +0800

    Setup Project

---
+++ LarichMorales.LM.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LarichMorales.LM.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
